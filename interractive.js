40c26eb6137d5be504edb432b3f0a72317a7be13
Alex Mozingo <alexmozingo@Alexs-MacBook-Air.local>
2020-10-02 11:05:17 -0400

    Graph

--- interractive.js
+++ interractive.js
@@ -1,7 +1,7 @@
-var drawBars1 = function(leagues, screen, graph, xScale, yScale)
+var drawBars1 = function(leagues, target, xScale, yScale1, graph)
     {
-        d3.select("svg")
-                .selectAll("rect")
+        target
+            .selectAll("rect")
                 .data(leagues)
                 .enter()
                 .append("rect")
@@ -11,13 +11,13 @@ var drawBars1 = function(leagues, screen, graph, xScale, yScale)
         })
                 .attr("y", function(pref)
                     {
-            return yScale(pref.Average)
+            return yScale1(pref.Average)
         })
                 .attr("width",xScale.bandwidth)
                 .attr("height", function(pref)
                         {
-                     console.log(yScale(pref.Average))
-                return screen.height-yScale(pref.Average)  
+                     console.log(yScale1(pref.Average))
+                return graph.height-yScale1(pref.Average)  
         })
             
 .on("mouseenter" ,function(pref)
@@ -48,7 +48,7 @@ var initGraph1 = function(leagues)
         
         var screen = {width:800,height:600}
         
-        var margins = {left:50,right:200,top:50,bottom:150}
+        var margins = {left:50,right:200,top:50,bottom:50}
     
     
     
@@ -58,22 +58,44 @@ var initGraph1 = function(leagues)
                 height:screen.height - margins.top-margins.bottom
             }
     
-    
-        var screen = {width:500, height:500}
+        var filterLeagues = leagues.filter(function(league)
+                {
+            if (league.League != "NFL")
+                {
+                    return true;
+                }
+            else  
+                {return false}
+        })
+        
+        
+        console.log(filterLeagues)
+        
+        
         d3.select("svg")
             .attr("width", screen.width)
             .attr("height", screen.height)
+        
+         
         var xScale = d3.scaleBand()
             .domain(["NBA","NHL"])
-            .range([0,screen.width])
+            .range([0,graph.width])
             .paddingInner(.2)
-        var yScale = d3.scaleLinear()
-            .domain([0,1])
-            .range([screen.height,0]);
-        drawBars1(leagues, screen, graph, xScale, yScale);
+        var yScale1 = d3.scaleLinear()
+            .domain([0,.03])
+            .range([graph.height,0]);
+        
+        var target = d3.select("svg")
+            .append("g")
+            .attr("id","graph")
+            .attr("transform",
+          "translate("+margins.left+","+
+                        margins.top+")");
+        
+        
+        drawBars1(filterLeagues, target, xScale, yScale1, graph);
         drawLabels1(graph, margins);
-        drawAxes1(graph,margins,xScale,yScale);
-
+        drawAxes1(graph,margins,xScale,yScale1);
     }
 
 var drawLabels1 = function(graph, margins)
@@ -93,24 +115,24 @@ var drawLabels1 = function(graph, margins)
         .text("Concussions")
         .classed("label", true)
         .attr("text-anchor", "middle")
-        .attr("transform", "translate(18," + (graph.height/2)+") rotate(270) ")
+        .attr("transform", "translate(12," + (graph.height/2)+") rotate(270) ")
     
     labels.append("text")
         .text("League")
         .classed("label", true)
         .attr("text-anchor", "middle")
         .attr("x", margins.left+(graph.width/2))
-        .attr("y", (graph.height))
+        .attr("y", graph.height+margins.top+margins.bottom-(10))
     
 }
 
 
 var drawAxes1 = function(graph,margins,
-                         xScale,yScale)
+                         xScale,yScale1)
 {
 
         var xAxis = d3.axisBottom(xScale);
-        var yAxis = d3.axisLeft(yScale);
+        var yAxis = d3.axisLeft(yScale1);
         
         var axes = d3.select("svg")
             .append("g")
@@ -136,9 +158,10 @@ var drawAxes1 = function(graph,margins,
 
 
 
-var drawBars2 = function(leagues, target, xScale, yScale)
+var drawBars2 = function(leagues, target, xScale, yScale, graph)
     {
-        target.selectAll("rect")
+        target
+            .selectAll("rect")
                 .data(leagues)
                 .enter()
                 .append("rect")
@@ -153,8 +176,8 @@ var drawBars2 = function(leagues, target, xScale, yScale)
                 .attr("width",xScale.bandwidth)
                 .attr("height", function(pref)
                         {
-                     console.log(yScale(pref.Average))
-                return screen.height-yScale(pref.Average)  
+                     console.log((pref.Average))
+                return graph.height-yScale(pref.Average)  
         })
             
 .on("mouseenter" ,function(pref)
@@ -183,9 +206,10 @@ var drawBars2 = function(leagues, target, xScale, yScale)
 var initGraph2 = function(leagues)
     {
         
+        
         var screen = {width:800,height:600}
         
-        var margins = {left:50,right:200,top:50,bottom:150}
+        var margins = {left:50,right:200,top:50,bottom:50}
     
     
     
@@ -195,9 +219,8 @@ var initGraph2 = function(leagues)
                 height:screen.height - margins.top-margins.bottom
             }
     
-       
     
-        var screen = {width:500, height:500}
+        
         d3.select("svg")
             .attr("width", screen.width)
             .attr("height", screen.height)
@@ -205,21 +228,21 @@ var initGraph2 = function(leagues)
          
         var xScale = d3.scaleBand()
             .domain(["NBA","NHL","NFL"])
-            .range([0,screen.width])
+            .range([0,graph.width])
             .paddingInner(.2)
         var yScale = d3.scaleLinear()
             .domain([0,1])
-            .range([screen.height,0]);
+            .range([graph.height,0]);
         
         var target = d3.select("svg")
             .append("g")
-            .attr("id","#graph")
+            .attr("id","graph")
             .attr("transform",
           "translate("+margins.left+","+
                         margins.top+")");
         
         
-        drawBars2(leagues, target, graph, xScale, yScale);
+        drawBars2(leagues, target, xScale, yScale, graph);
         drawLabels2(graph, margins);
         drawAxes2(graph,margins,xScale,yScale);
 
@@ -249,7 +272,7 @@ var drawLabels2 = function(graph, margins)
         .classed("label", true)
         .attr("text-anchor", "middle")
         .attr("x", margins.left+(graph.width/2))
-        .attr("y", (graph.height))
+        .attr("y", graph.height+margins.top+margins.bottom)
     
 }
 
@@ -274,11 +297,23 @@ var drawAxes2 = function(graph,margins,
 }
 
 
+var clickGraph = function(leagues)
+    {
+        d3.select("#graph")
+            .on("click", function()
+                {
+            d3.selectAll("svg *")
+            .remove()
+            initGraph2(leagues);
+        })
+    }
+
 
 var successFCN = function(leagues)
 {
     console.log("relative",leagues);
-    initGraph2(leagues);
+    initGraph1(leagues);
+    clickGraph(leagues);
 }
 
 var failFCN = function(error)
