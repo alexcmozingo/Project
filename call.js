bda356d2832beb166fc528f7f50e9a26a4a04375
Alex Mozingo <alexmozingo@Alexs-MacBook-Air.local>
2020-10-06 22:06:39 -0400

    Call

--- call.js
+++ call.js
@@ -1,27 +1,23 @@
-var drawBars1 = function(actions, target, xScale, yScale1, graph)
+var drawBars5 = function(contacts, target3, xScale, yScale5, graph)
     {
-        target
+        target3
             .selectAll("rect")
-                .data(actions)
+                .data(contacts)
                 .enter()
                 .append("rect")
-                .attr("id", function(pref)
-                        {
-                        console.log(pref)
-                    return pref.Action
-        })
+                .attr("id", "contacts")
                 .attr("x", function(pref)
                     {
-            return xScale(pref.Action)
+            return xScale(pref.Part)
         })
                 .attr("y", function(pref)
                     {
-            return yScale1(pref.Concussions)
+            return yScale5(pref.Percentage)
         })
                 .attr("width",xScale.bandwidth)
                 .attr("height", function(pref)
                         {
-                return graph.height-yScale1(pref.Concussions)  
+                return graph.height-yScale5(pref.Percentage)  
         })
             
 .on("mouseenter" ,function(pref)
@@ -36,9 +32,9 @@ var drawBars1 = function(actions, target, xScale, yScale1, graph)
         .style("left",xPos+"px")
         
         
-        d3.select("#action")
-        .text(pref.Concussions);
-      })
+        d3.select("#league")
+        .text(pref.Percentage);
+        })
     .on("mouseleave",function()
     {
         d3.select("#tooltip")    
@@ -47,159 +43,7 @@ var drawBars1 = function(actions, target, xScale, yScale1, graph)
     }
 
 
-var initGraph1 = function(actions)
-    {
-        
-        var screen = {width:800,height:600}
-        
-        var margins = {left:50,right:200,top:50,bottom:50}
-    
-    
-    
-        var graph = 
-            {
-                width:screen.width-margins.left-margins.right,
-                height:screen.height - margins.top-margins.bottom
-            }
-        
-        
-        d3.select("svg")
-            .attr("width", screen.width)
-            .attr("height", screen.height)
-        
-         
-        var xScale = d3.scaleBand()
-            .domain(["Tackling", "Tackled", "Blocking", "Blocked"])
-            .range([0,graph.width])
-            .paddingInner(.2)
-        var yScale1 = d3.scaleLinear()
-            .domain([0,500])
-            .range([graph.height,0]);
-        
-        var target = d3.select("svg")
-            .append("g")
-            .attr("id","graph")
-            .attr("transform",
-          "translate("+margins.left+","+
-                        margins.top+")");
-        
-        
-        drawBars1(actions, target, xScale, yScale1, graph);
-        drawLabels1(graph, margins);
-        drawAxes1(graph,margins,xScale,yScale1);
-    }
-
-var drawLabels1 = function(graph, margins)
-{
-    var labels = d3.select("svg")
-        .append("g")
-        .classed("labels", true)
-        
-    labels.append("text")
-        .text("Concussions by Action")
-        .classed("title", true)
-        .attr("text-anchor", "middle")
-        .attr("x", margins.left+(graph.width/2))
-        .attr("y", margins.top+(10))
-    
-    labels.append("text")
-        .text("Concussions")
-        .classed("label", true)
-        .attr("text-anchor", "middle")
-        .attr("transform", "translate(12," + (graph.height/2)+") rotate(270) ")
-    
-    labels.append("text")
-        .text("Action")
-        .classed("label", true)
-        .attr("text-anchor", "middle")
-        .attr("x", margins.left+(graph.width/2))
-        .attr("y", graph.height+margins.top+margins.bottom-(10))
-    
-}
-
-
-var drawAxes1 = function(graph,margins,
-                         xScale,yScale1)
-{
-
-        var xAxis = d3.axisBottom(xScale);
-        var yAxis = d3.axisLeft(yScale1);
-        
-        var axes = d3.select("svg")
-            .append("g")
-        axes.append("g")
-            .attr("transform", "translate("+margins.left+","+(margins.top+graph.height)+")")
-            .call(xAxis)
-        axes.append("g")
-            .attr("transform", "translate("+margins.left+","+(margins.top)+")")
-            .call(yAxis)
-
- 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var drawBars2 = function(actions, target, xScale, yScale, graph)
-    {
-        target
-            .selectAll("rect")
-                .data(actions)
-                .enter()
-                .append("rect")
-                .attr("x", function(pref)
-                    {
-            return xScale(pref.Action)
-        })
-                .attr("y", function(pref)
-                    {
-            return yScale(pref.Concussions)
-        })
-                .attr("width",xScale.bandwidth)
-                .attr("height", function(pref)
-                        {
-                     console.log(pref)
-                return graph.height-yScale(pref.Concussions)  
-        })
-            
-.on("mouseenter" ,function(pref)
-    {
-        
-      var xPos = d3.event.pageX;
-      var yPos = d3.event.pageY;
-      
-        d3.select("#tooltip")
-        .classed("hidden",false)
-        .style("top",yPos+"px")
-        .style("left",xPos+"px")
-        
-        
-        d3.select("#action")
-        .text(pref.Average);
-      })
-    .on("mouseleave",function()
-    {
-        d3.select("#tooltip")    
-        .classed("hidden",true);
-        })
-    }
-
-
-var initGraph2 = function(leagues)
+var initGraph5 = function(contacts)
     {
         
         
@@ -217,54 +61,53 @@ var initGraph2 = function(leagues)
     
     
         
-        d3.select("svg")
+        d3.select("#thirdGraph")
             .attr("width", screen.width)
             .attr("height", screen.height)
         
          
         var xScale = d3.scaleBand()
-            .domain(["Tackling", "Blocking"])
+            .domain(["Helmet", "Shoulder", "Forearm/Elbow", "Knee", "Thigh", "Foot", "Playing Surface", "Unknown"])
             .range([0,graph.width])
             .paddingInner(.2)
-        var yScale = d3.scaleLinear()
+        var yScale5 = d3.scaleLinear()
             .domain([0,1])
             .range([graph.height,0]);
         
-        var target = d3.select("svg")
+         var target3 = d3.select("#thirdGraph")
             .append("g")
-            .attr("id","graph")
             .attr("transform",
           "translate("+margins.left+","+
                         margins.top+")");
         
         
-        drawBars2(actions, target, xScale, yScale, graph);
-        drawLabels2(graph, margins);
-        drawAxes2(graph,margins,xScale,yScale);
+        drawBars5(contacts, target3, xScale, yScale5, graph);
+        drawLabels5(graph, margins);
+        drawAxes5(graph,margins,xScale,yScale5);
 
     }
 
-var drawLabels2 = function(graph, margins)
+var drawLabels5 = function(graph, margins)
 {
-    var labels = d3.select("svg")
+    var labels = d3.select("#thirdGraph")
         .append("g")
         .classed("labels", true)
         
     labels.append("text")
-        .text("Concussions Per Game")
+        .text("Body Part that Caused the Concussion")
         .classed("title", true)
         .attr("text-anchor", "middle")
         .attr("x", margins.left+(graph.width/2))
         .attr("y", margins.top+(10))
     
     labels.append("text")
-        .text("Concussions")
+        .text("Percentage")
         .classed("label", true)
         .attr("text-anchor", "middle")
         .attr("transform", "translate(18," + (graph.height/2)+") rotate(270) ")
     
     labels.append("text")
-        .text("League")
+        .text("Body Part")
         .classed("label", true)
         .attr("text-anchor", "middle")
         .attr("x", margins.left+(graph.width/2))
@@ -273,14 +116,14 @@ var drawLabels2 = function(graph, margins)
 }
 
 
-var drawAxes2 = function(graph,margins,
-                         xScale,yScale)
+var drawAxes5 = function(graph,margins,
+                         xScale,yScale5)
 {
 
         var xAxis = d3.axisBottom(xScale);
-        var yAxis = d3.axisLeft(yScale);
+        var yAxis = d3.axisLeft(yScale5);
         
-        var axes = d3.select("svg")
+        var axes = d3.select("#thirdGraph")
             .append("g")
         axes.append("g")
             .attr("transform", "translate("+margins.left+","+(margins.top+graph.height)+")")
@@ -293,24 +136,15 @@ var drawAxes2 = function(graph,margins,
 }
 
 
-var clickGraph = function(leagues)
-    {
-        d3.select("#button")
-            .on("click", function()
-                {
-            d3.selectAll("svg *")
-            .remove()
-            initGraph2(leagues);
-        })
-    }
 
 
 
 
-var successFCN = function(actions)
+
+var successFCN = function(contacts)
 {
-    console.log("actions",actions);
-    initGraph1(actions);
+    console.log("contacts",contacts);
+    initGraph5(contacts);
 }
 
 var failFCN = function(error)
@@ -318,6 +152,6 @@ var failFCN = function(error)
     console.log("error",error);
 }
 
-var actionPromise = d3.csv("actions.csv")
+var actionPromise = d3.csv("contacts.csv")
 
 actionPromise.then(successFCN,failFCN);
